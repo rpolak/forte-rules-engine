c5285cbcdf8f19b675c929f53f44ff57648916ba
Oscar Serna <oscar.eduardo.serna@gmail.com>
2025-09-10 12:59:26 -0500

    created _unapplyPolicy internal function and placed it in facetUtils

--- src/engine/facets/RulesEnginePolicyFacet.sol
+++ src/engine/facets/RulesEnginePolicyFacet.sol
@@ -178,36 +178,7 @@ contract RulesEnginePolicyFacet is FacetCommonImports {
         address _contractAddress,
         uint256[] calldata _policyIds
     ) external callingContractAdminOnly(_contractAddress, msg.sender) {
-        if (_contractAddress == address(0)) revert(ZERO_ADDRESS);
-        // Load the policy association data from storage
-        PolicyAssociationStorage storage data = lib._getPolicyAssociationStorage();
-        // Get the currently applied policyIds
-        uint256[] memory allPolicyIds = data.contractPolicyIdMap[_contractAddress];
-        // Blow away the contract to policyId association data in order to keep the associated id's array length in line with the amount of policies associated.
-        delete data.contractPolicyIdMap[_contractAddress];
-        bool found;
-        for (uint256 i = 0; i < allPolicyIds.length; i++) {
-            found = false;
-            for (uint256 j = 0; j < _policyIds.length; j++) {
-                // if the id exists in the unapply list, don't carry it forward
-                if (allPolicyIds[i] == _policyIds[j]) found = true;
-            }
-            if (!found) data.contractPolicyIdMap[_contractAddress].push(allPolicyIds[i]);
-        }
-
-        // Loop through the policyId to contract arrays, clear them and add back only the correct addresses
-        for (uint256 i = 0; i < _policyIds.length; i++) {
-            // Get the currently applied policyIds
-            address[] memory allContracts = data.policyIdContractMap[_policyIds[i]];
-
-            // Blow away the policyId to contract association data in order to keep the associated id's array length in line with the amount of contracts associated.
-            delete data.policyIdContractMap[_policyIds[i]];
-            for (uint256 j = 0; j < allContracts.length; j++) {
-                // if the address is not the current address, add it back to the array.
-                if (allContracts[j] != _contractAddress) data.policyIdContractMap[_policyIds[i]].push(allContracts[j]);
-            }
-        }
-        emit PolicyUnapplied(_policyIds, _contractAddress);
+        _unapplyPolicy(_contractAddress, _policyIds);
     }
 
     /**
